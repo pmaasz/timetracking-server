502fe292b6bd3f027e7b9202845e6e6f678869fc
Philip Maaß <PhilipMaasz@aol.com>
2022-07-10 01:38:32 +0200

    adds functionality to PauseRepository

--- src/Repository/PauseRepository.php
+++ src/Repository/PauseRepository.php
@@ -2,6 +2,9 @@
 
 namespace Timetracking\Server\Repository;
 
+use Timetracking\Server\Model\Pause;
+use Timetracking\Server\Service\Database;
+
 /**
  * Created by PhpStorm.
  * Author: Philip Maaß
@@ -11,5 +14,76 @@ namespace Timetracking\Server\Repository;
  */
 class PauseRepository
 {
+    /**
+     * @param Pause $pause
+     *
+     * @return mixed
+     */
+    public function create(Pause $pause)
+    {
+        return Database::getInstance()->insert("INSERT INTO pause SET pause_start = :pauseStart, pause_end = :pauseEnd, pause = :pause", [
+            'pauseStart' => $pause->getPauseStart(),
+            'pauseEnd' => $pause->getPauseEnd(),
+            'pause' => $pause->getPause(),
+        ]);
+    }
 
+    /**
+     * @param Pause $workday
+     *
+     * @return mixed
+     */
+    public function update(Pause $pause)
+    {
+        return Database::getInstance()->insert("UPDATE pause SET  WHERE id = :id", [
+            'pauseStart' => $pause->getPauseStart(),
+            'pauseEnd' => $pause->getPauseEnd(),
+            'pause' => $pause->getPause(),
+            'id' => $pause->getId(),
+        ]);
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return mixed
+     */
+    public function delete($id)
+    {
+        return Database::getInstance()->query('DELETE FROM pause WHERE id = :id', [
+            'id' => $id,
+        ]);
+    }
+
+    /**
+     * @param Pause $pause
+     *
+     * @return bool
+     */
+    public function persist(Pause $pause)
+    {
+        if($pause->getId() !== null)
+        {
+            return $this->update($pause);
+        }
+
+        return $this->create($pause);
+    }
+
+    /**
+     * @param $data
+     *
+     * @return Pause
+     */
+    private function arrayToObject($data)
+    {
+        $object = new Pause();
+
+        $object->setId($data['id']);
+        $object->setPauseStart($data['pause_start']);
+        $object->setPauseEnd($data['pause_end']);
+        $object->setPause($data['pause']);
+
+        return $object;
+    }
 }
