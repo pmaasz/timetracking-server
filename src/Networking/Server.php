88384cf42927de860d501e19b65b40e27f2dc9bc
Philip Maaß <PhilipMaasz@aol.com>
2022-07-10 00:53:30 +0200

    loads config

--- src/Networking/Server.php
+++ src/Networking/Server.php
@@ -2,6 +2,9 @@
 
 srand(\Timetracking\Server\Service\RandomService::getSeed() + time());
 
+//load configuration
+\Timetracking\Server\Service\ConfigService::getInstance()->load(__DIR__ . '/../config/config.json.dist');
+
 // http sse formatted broadcast stream
 $broadcastStream = new \React\Stream\ThroughStream(function ($data) {
     return $data;
@@ -9,7 +12,6 @@ $broadcastStream = new \React\Stream\ThroughStream(function ($data) {
 
 $sseHelper = new \Timetracking\Server\Networking\SSEHelper();
 $http = new React\Http\HttpServer(function (Psr\Http\Message\ServerRequestInterface $request) use($sseHelper, $broadcastStream) {
-
     try {
         $httpPath = $request->getUri()->getPath();
 
@@ -26,7 +28,10 @@ $http = new React\Http\HttpServer(function (Psr\Http\Message\ServerRequestInterf
         print_r($e->getTraceAsString());
 
         return new React\Http\Message\Response(
-            React\Http\Message\Response::STATUS_INTERNAL_SERVER_ERROR,[],$e->getMessage()
+            React\Http\Message\Response::STATUS_INTERNAL_SERVER_ERROR,
+            [
+            ],
+            $e->getMessage()
         );
     }
 });
