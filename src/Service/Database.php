3747ad274c6fd1b60eb2b0d76ce354d36dcb6b97
Philip Maaß <PhilipMaasz@aol.com>
2022-07-10 00:46:03 +0200

    adds a Database class to connect to the DB

--- src/Service/Database.php
+++ src/Service/Database.php
@@ -11,5 +11,75 @@ namespace Timetracking\Server\Service;
 
 class Database
 {
+    use Singleton;
 
+    /**
+     * @var \PDO
+     */
+    private $connection;
+
+    /**
+     * connects to Database
+     */
+    private function connect()
+    {
+        $config = ConfigService::getInstance()->get('database');
+        $this->connection = new \PDO($this->getDSN($config), $config['user'], $config['password']);
+    }
+
+    /**
+     * @param $query
+     * @param array $parameters
+     *
+     * @return bool
+     */
+    public function insert($query, array $parameters = array())
+    {
+        $statement = $this->connection->prepare($query);
+
+        foreach($parameters as $key => $value)
+        {
+            $statement->bindValue(':'.$key, $value);
+        }
+
+        return $statement->execute();
+    }
+
+    /**
+     * @param $query
+     * @param array $parameters
+     *
+     * @return array
+     */
+    public function query($query, array $parameters = array())
+    {
+        $statement = $this->connection->prepare($query);
+
+        foreach($parameters as $key => $value)
+        {
+            $statement->bindValue(':'.$key, $value);
+        }
+
+        $statement->execute();
+
+        return $statement->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Database constructor.
+     */
+    protected function __construct()
+    {
+        $this->connect();
+    }
+
+    /**
+     * @param $config
+     *
+     * @return string
+     */
+    private function getDSN($config)
+    {
+        return sprintf("%s:host=%s;dbname=%s", $config['driver'], $config['host'], $config['dbname']);
+    }
 }
