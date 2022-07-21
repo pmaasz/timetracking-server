774e1cbb1fb48c92658a87f65cdfdf75c83585a6
Philip Maaß <PhilipMaasz@aol.com>
2022-07-21 16:46:25 +0200

    implements calculation functions in TimeService

--- src/Service/TimeService.php
+++ src/Service/TimeService.php
@@ -9,7 +9,54 @@
 
 namespace Timetracking\Server\Service;
 
+use Timetracking\Server\Model\Pause;
+use Timetracking\Server\Model\WorkDay;
+
 class TimeService
 {
+    /**
+     * @param WorkDay $workday
+     *
+     * @return WorkDay
+     */
+    public function calculateTotalHours($workday)
+    {
+        $totalHours = 0;
+        foreach ($workday->getTimeEntries() as $timeEntry) {
+            $totalHours += $timeEntry->getHours();
+        }
 
+        $workday->setHoursTotal($totalHours);
+
+        return $workday;
+    }
+
+    /**
+     * @param WorkDay $workday
+     *
+     * @return WorkDay
+     */
+    public function calculateTotalPause($workday)
+    {
+        $totalPause = 0;
+        foreach ($workday->getPauses() as $pause) {
+            $totalPause += $pause->getPause();
+        }
+
+        $workday->setPauseTotal($totalPause);
+
+        return $workday;
+    }
+
+    /**
+     * @param Pause $pause
+     *
+     * @return Pause
+     */
+    public function calculatePause($pause)
+    {
+        $pause->setPause($pause->getPauseEnd() - $pause->getPauseStart());
+
+        return $pause;
+    }
 }
